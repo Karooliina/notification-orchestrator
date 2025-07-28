a02b3ec79a51ff2c36175cdf81ea5cf88395d5dd
Karooliina <karooliina.m@gmail.com>
2025-07-28 23:02:12 +0200

    Swagger docs

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import cors from 'cors';
 import { createUserDNDTable } from './db/createUserDNDTable';
 import { createUserNotificationTable } from './db/createUserNotificationTable';
 import { authorizeRequest } from './middleware/authMiddleware';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './api/v1/controller/docs/apiDocs.json';
 
 (async () => {
   const app = express();
@@ -21,12 +23,14 @@ import { authorizeRequest } from './middleware/authMiddleware';
     res.status(200).json({ success: true, data: 'Healthy' });
   });
 
+  app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   app.use('/api/v1/notifications', authorizeRequest, notificationsRouter);
 
   app.use('/api/v1/user-preferences', authorizeRequest, userPreferencesRouter);
 
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
   });
 
   console.log('Creating tables');
